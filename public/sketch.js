5d1b6f15e109cf8b7b211e3c1cf41d38a3423fe3
MsQCompSci <46223372+MsQCompSci@users.noreply.github.com>
2024-01-02 13:46:19 -0500

    base web editor starter code

--- public/sketch.js
+++ public/sketch.js
@@ -1,23 +1,5 @@
-
 function setup() {
     createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
-
-  //variable for oscialtor object
-let osc;
-
-//varable for frequency
-let freq = 262;
-
-function setup() {
-    createCanvas(400, 400);
-
-    //initialize oscillator
-    osc = new Oscillator(freq);
   }
   
   function draw() {
