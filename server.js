d01b827ac5376c1d36d70280fdb198b778fb0f4a
MsQCompSci <46223372+MsQCompSci@users.noreply.github.com>
2024-04-10 12:52:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,19 @@ let express = require('express');
 // Create the Express app
 let app = express();
 
-//Initialize file system module
-let fs = require('fs');
-
 // Set the port used for server traffic.
 let port = 3000;
 
-// Serve static files from 'public' directory
+// Middleware to serve static files from 'public' directory
 app.use(express.static('public'));
 
 //Step 3 code goes here
-//initialize file system module
-// Serve static files from 'songs' directory
+//Initialize file system module
+// Middleware to serve static files from 'songs' directory
 // Middleware for parsing JSON files
-// API endpoint to get list of songs
-//Get list of file names and parse as json object
+// API endpoint to get songs folder
+// File directory endpoint to get list of file names
+// Read file names into a JSON object - throw error otherwise
 
 //Run server at port
 app.listen(port, () => {
